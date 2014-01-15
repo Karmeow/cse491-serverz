cf3d39fb86bd9d840bd0263f875763c1cf03d5ee
Kevin Karabon <karabonk@arctic.cse.msu.edu>
2014-01-14 19:46:00 -0500

    Added an HTTP response and some html to display

--- server.py
+++ server.py
@@ -15,9 +15,8 @@ s.listen(5)                 # Now wait for client connection.
 
 print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
-    # Establish connection with client.    
+    # Establish connection with client.
     c, (client_host, client_port) = s.accept()
     print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("good bye.")
+    c.send("HTTP/1.0 200 OK\nContent-type: text/html\n\n<h1>Hello, World</h1> This is Karmeow's Web server.")
     c.close()
