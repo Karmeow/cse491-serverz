e6c538fdc7dbc7f1f41a8a5d6538ac37fcda00f7
Kevin Karabon <karabonk@arctic.cse.msu.edu>
2014-02-09 18:04:44 -0500

    hw5

--- server.py
+++ server.py
@@ -5,14 +5,17 @@ import socket
 import time
 import cgi
 import StringIO
-import jinja2
-#
+import app
+
 def handle_connection(conn):
+
+    def start_response(status, response_headers):
+        return closure()
+        
     # Start connection and receive data through headers
     data = conn.recv(1)
     while data[-4:] != '\r\n\r\n':
         data += conn.recv(1)
-
 
     # Repurpose code used earlier to parse headers into a dict
     reqc = StringIO.StringIO(data)
@@ -28,55 +31,36 @@ def handle_connection(conn):
 
     req = data.split(" ")
 
-    # Parse the path and add query dictionary to the render vars
-    url = urlparse.urlparse(req[1])
-    renvars = urlparse.parse_qs(url.query)
-
     # Extra content if the type is post
     content = ''
     if data.startswith('POST '):
         # Receive the content data, based off the content length header
         while len(content) < int(headers['content-length']):
             content += conn.recv(1)
-    print (data,)
-    print (content,)
+
+    
+    path = urlparse.urlparse(req[1])
+    # Create the environ dict for the app
     environ = {}
-    environ['REQUEST_METHOD'] = 'POST'
+    environ['REQUEST_METHOD'] = req[0]
+    environ['PATH_INFO'] = path.path
+    environ['QUERY_STRING'] = path.query
+    if 'content-type' in headers:
+        environ['CONTENT_TYPE'] = headers['content-type']
+    if 'content-length' in headers:
+        environ['CONTENT_LENGTH'] = headers['content-length']
+    environ['wsgi.input'] = StringIO.StringIO(content)
+    
+    def start_response(status, response_headers):
+        conn.send('HTTP/1.0 %s' % status)
+        conn.send('\r\n')
+        conn.send('%s : %s' % (response_headers[0][0], response_headers[0][1]))
+        conn.send('\r\n\r\n')
 
-    fs = cgi.FieldStorage(fp = StringIO.StringIO(content), headers=headers \
-                              , environ=environ)
-    print fs.keys()
-    # Add post data to the render vars
-    renvars.update(dict([(x, [fs[x].value]) for x in fs.keys()]))
+    result = app.simple_app(environ, start_response)
 
-    # Dict of current possible paths
-    pages = { '/' : 'index.html', \
-              '/content' : 'content.html', \
-              '/image' : 'image.html', \
-              '/file' : 'file.html', \
-              '/form' : 'form.html', \
-              '/submit' : 'submit.html' }
-
-    loader = jinja2.FileSystemLoader('./templates')
-    env = jinja2.Environment(loader=loader)
-    status = 'HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n'
-
-    if data.startswith('POST '):
-        template = env.get_template('submit.html')
-
-    elif url.path in pages:
-        template = env.get_template(pages[url.path])
-
-    else:
-        template = env.get_template('404.html')
-        status = 'HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\n\r\n'
-
-
-    status += template.render(renvars)
-    conn.send(status)
-
+    conn.send(result[0])
     conn.close()
-
 
 def main():
     s = socket.socket()         # Create a socket object
@@ -94,6 +78,7 @@ def main():
         # Establish connection with client.
         c, (client_host, client_port) = s.accept()
         print 'Got connection from', client_host, client_port
+
         handle_connection(c)
 
 
--- test_server.py
+++ test_server.py
@@ -25,80 +25,50 @@ class FakeConnection(object):
     def close(self):
         self.is_closed = True
 
-# Test a basic GET call.
-# ^ ~CAT : This is the ONLY comment in the file, and it just seems out of place.
-# Either document them all, or document none. Obviously the former is more
-# preferred.
-# Here's an optional suggested format for function declarations:
-# def function_name(params):
-#     "Function Documentation"
-#     # Code goes here
-#
-# If you use this format, I think you'll start to like it quite a bit. You can
-# also use triple-quoted strings for the function documentation, just like ctb
-# did with the documentation on the FakeConnection class above.
-# Again, just a suggestion, but either way, BE CONSISTENT is the big deal.
-
 def test_handle_connection():
     conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
-    expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-Type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      '\nThis is Karmeow\'s Web server.' + \
-                      '\n<p><a href="/content">Content :)</a></p>' + \
-                      '\n<p><a href="/file">File :)</a></p>' + \
-                      '\n<p><a href="/image">Image :(</a></p>' + \
-                      '\n<p><a href="/form">Form</a></p>'
-
+    expected_return = 'HTTP/1.0 200 OK'
     server.handle_connection(conn)
-
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    splitconn = conn.sent.split('\r\n')[0]
+    
+    assert splitconn == expected_return, 'Got: %s' %(repr(conn.sent),)
 
 def test_handle_content_connection():
     conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
-    expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-Type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<b>Ya\'ll requested some content</b>'
+    expected_return = 'HTTP/1.0 200 OK'
 
     server.handle_connection(conn)
-
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    splitconn = conn.sent.split('\r\n')[0]
+    
+    assert splitconn == expected_return, 'Got: %s' %(repr(conn.sent),)
 
 def test_handle_file_connection():
     conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
-    expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-Type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<b>Ya\'ll requested some file</b>'
+    expected_return = 'HTTP/1.0 200 OK'
 
     server.handle_connection(conn)
-    print (expected_return,)
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    splitconn = conn.sent.split('\r\n')[0]
+    
+    assert splitconn == expected_return, 'Got: %s' %(repr(conn.sent),)
 
 def test_handle_image_connection():
     conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
-    expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-Type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<b>Ya\'ll requested some image</b>'
+    expected_return = 'HTTP/1.0 200 OK'
 
     server.handle_connection(conn)
-
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    splitconn = conn.sent.split('\r\n')[0]
+    
+    assert splitconn == expected_return, 'Got: %s' %(repr(conn.sent),)
 
 def test_form_connection():
     conn = FakeConnection('GET /submit?firstname=Robert&lastname=Paulson' + \
                           ' HTTP/1.0\r\n\r\n')
-    expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-Type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello Mr. Robert Paulson</h1>'
+    expected_return = 'HTTP/1.0 200 OK'
 
     server.handle_connection(conn)
-
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    splitconn = conn.sent.split('\r\n')[0]
+    
+    assert splitconn == expected_return, 'Got: %s' %(repr(conn.sent),)
 
 def test_app_post_connection():
     conn = FakeConnection('POST / HTTP/1.0\r\n' + \
@@ -108,15 +78,13 @@ def test_app_post_connection():
             '\r\n'
             'firstname=Robert&lastname=Paulson')
 
-    expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-Type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello Mr. Robert Paulson</h1>'
+    expected_return = 'HTTP/1.0 200 OK'
 
 
     server.handle_connection(conn)
-
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    splitconn = conn.sent.split('\r\n')[0]
+    
+    assert splitconn == expected_return, 'Got: %s' %(repr(conn.sent),)
 
 def test_multi_post_connection():
     conn = FakeConnection('POST /submit HTTP/1.1\r\n' + \
@@ -131,27 +99,30 @@ def test_multi_post_connection():
         'Content-Disposition: form-data; name="lastname"\r\n\r\n' +
         'Paulson\r\n------WebKitFormBoundaryeDbkALVq8cSTv4RB--\r\n')
 
-    expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-Type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello Mr. Robert Paulson</h1>'
+    expected_return = 'HTTP/1.0 200 OK'
 
     server.handle_connection(conn)
-
-    assert conn.sent == expected_return, 'Got: %s' %(repr(conn.sent),)
+    splitconn = conn.sent.split('\r\n')[0]
+    
+    assert splitconn == expected_return, 'Got: %s' %(repr(conn.sent),)
 
 def test_bad_connection():
     conn = FakeConnection('GET /autobiography HTTP/1.0\r\n\r\n')
 
-    expected_return = 'HTTP/1.0 404 Not Found\r\n' + \
-                      'Content-Type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>404 Error - Not Found</h1>'
+    expected_return = 'HTTP/1.0 404 Not Found'
 
     server.handle_connection(conn)
+    splitconn = conn.sent.split('\r\n')[0]
+    
+    assert splitconn == expected_return, 'Got: %s' %(repr(conn.sent),)
+    
+def test_put_connection():
+    conn = FakeConnection('PUT / HTTP/1.0\r\n\r\n')
+    
+    expected_return = 'HTTP/1.0 404 Not Found'
+    
+    server.handle_connection(conn)
+    splitconn = conn.sent.split('\r\n')[0]
+    
+    assert splitconn == expected_return, 'Got: %s' %(repr(conn.sent),)
 
-    assert conn.sent == expected_return, 'Got: %s' %(repr(conn.sent),)
-
-
-# ~CAT : You didn't test for failure conditions...
-# What happens if a PUT or DELETE comes in? Or a page that doesn't exist?
